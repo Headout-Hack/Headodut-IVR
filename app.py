9ab23fd66b9eb60df8e8ff55e30b70dac4741cf7
raghavddps2 <raghav.ddps2@gmail.com>
2020-11-07 18:58:01 +0530

    Adding code

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def voice():
           
         )
         response.record(
-            action= 'http://2e8d390ba5e3.ngrok.io/answer',
+            action= 'https://hackout-helpline.herokuapp.com/answer',
             method='GET',
             finish_on_key='*',
             transcribe=True,
@@ -102,7 +102,7 @@ def gatherConfirm():
             response.say(en['message5'],voice='Polly.Aditi',language="hi-IN",)
             response.say(en['message6'],voice='Polly.Aditi',language="hi-IN")
             response.record(
-                action= 'http://2e8d390ba5e3.ngrok.io/gatherAddressCode',
+                action= 'https://hackout-helpline.herokuapp.com/gatherAddressCode',
                 method='GET',
                 finish_on_key='*',
                 transcribe=True,
